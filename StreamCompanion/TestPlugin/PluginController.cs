b0b5f76b5ad853c3921bbfdb7f0b940a387582ac
ivanovk <ivanovk@krista.ru>
2022-02-18 17:29:54 +0300

    Заготовка под плагин

---
+++ StreamCompanion/TestPlugin/PluginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestPlugin
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PluginController : ControllerBase
+    {
+        public PluginController()
+        {
+        }
+
+        [HttpGet("Version")]
+        public object Version()
+        {
+            return "Plugin Controller v 1.0";
+        }
+    }
+}
