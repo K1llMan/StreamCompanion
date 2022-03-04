7b966cf74078a0b6e77217192219dad491f34e42
ivanovk <ivanovk@krista.ru>
2022-03-04 10:17:20 +0300

    Заготовка для шины событий

---
+++ StreamCompanion/CompanionPlugin/Classes/StreamEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace CompanionPlugin.Classes;
+
+public class StreamEvent
+{
+    public string Name { get; set; }
+    public string Date { get; set; }
+}
---
+++ StreamCompanion/CompanionPlugin/Services/StreamEventsService.cs
@@ -0,0 +1,32 @@
+﻿using CompanionPlugin.Classes;
+using CompanionPlugin.Interfaces;
+
+namespace CompanionPlugin.Services;
+
+public class StreamEventsService : IEventBus
+{
+    #region Поля
+
+    private Dictionary<string, List<EventBusHandler>> subscriptions = new ();
+
+    #endregion Поля
+
+    #region Основные функции
+
+    public void Publish(StreamEvent streamEvent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Subscribe(string eventName, EventBusHandler handler)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Unsubscribe(string eventName, EventBusHandler handler)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion Основные функции
+}
